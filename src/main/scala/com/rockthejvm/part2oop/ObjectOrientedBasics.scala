f2565b471edad4a716bcec4be984384dc1f943c3
dhuff <dallinhuff@gmail.com>
2023-06-19 11:23:20 -0600

    complete oo basics exercises

--- src/main/scala/com/rockthejvm/part2oop/ObjectOrientedBasics.scala
+++ src/main/scala/com/rockthejvm/part2oop/ObjectOrientedBasics.scala
@@ -28,8 +28,61 @@ object ObjectOrientedBasics {
   val aliceGreetsDaniel = aPerson.greet("Daniel")
   val aliceSaysHi = aPerson.greet()
 
+  // Exercise 1
+  class Writer(firstName: String, lastName: String, val year: Int) {
+    def fullName: String = s"$firstName $lastName"
+  }
+
+  class Novel(name: String, year: Int, author: Writer) {
+    def authorAge: Int = year - author.year
+    def isWrittenBy(author: Writer): Boolean = this.author == author
+    def copy(newYear: Int) = new Novel(name, newYear, author)
+  }
+
+  // Exercise 2
+  class Counter(count: Int = 0) {
+    def increment(): Counter = new Counter(count + 1)
+    def increment(n: Int): Counter =
+      if n <= 0 then this
+      else increment().increment(n - 1)
+
+    def decrement(): Counter =
+      if count <= 0 then this
+      else new Counter(count - 1)
+
+    def decrement(n: Int): Counter =
+      if count <= 0 || n <= 0 then this
+      else decrement().decrement(n - 1)
+
+    def print(): Unit = println(s"The count is: $count")
+  }
+
   def main(args: Array[String]): Unit = {
     println(aliceGreetsDaniel)
     println(aliceSaysHi)
+
+    val charlesDickens = new Writer("Charles", "Dickens", 1812)
+    println(charlesDickens.fullName)
+    val impostor = new Writer("Charles", "Dickens", 1812)
+
+    val novel = new Novel("Great Expectations", 1860, charlesDickens)
+    println(novel.authorAge)
+
+    val writtenByDickens = novel.isWrittenBy(charlesDickens)
+    val writtenByImpostor = novel.isWrittenBy(impostor)
+    println(writtenByDickens)
+    println(writtenByImpostor)
+
+    val newEdition = novel.copy(1871)
+    println(newEdition.authorAge)
+
+    val counter = new Counter()
+    counter.print() // 0
+    counter.increment().print() // 1
+    counter.increment() // new Counter(1)
+    counter.print() // 0
+    val otherCounter = counter.increment(6)
+    otherCounter.print()
+    otherCounter.decrement(3).print()
   }
 }
