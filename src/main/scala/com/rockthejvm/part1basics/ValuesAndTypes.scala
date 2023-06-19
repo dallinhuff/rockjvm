088c6ec1f8f38be212e06b9df73a5c56c8109696
dhuff <dallinhuff@gmail.com>
2023-06-19 10:07:05 -0600

    complete vals & types

---
+++ src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
@@ -0,0 +1,23 @@
+package com.rockthejvm.part1basics
+
+object ValuesAndTypes {
+
+  // values (immutable, type inferred)
+  val meaningOfLife = 42
+
+  // common/primitives
+  val aBoolean = true
+  val aChar = 'a'
+  val anInt = 62 // 4-bytes
+  val aShort: Short = 5263 // 2-bytes
+  val aLong = 78L // 8-bytes, can use L suffix to differentiate
+  val aFloat = 2.4f // 4 bytes
+  val aDouble = 3.14
+
+  // string
+  val aString = "word"
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
